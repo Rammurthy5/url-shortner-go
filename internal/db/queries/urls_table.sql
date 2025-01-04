388e7373b6a08f81ed7385489b65e72eb422849d
Rammurthy5 <ram.sanjiv@gmail.com>
2025-01-04 15:10:52 +0000

    sqlc updates

--- internal/db/queries/urls_table.sql
+++ internal/db/queries/urls_table.sql
@@ -3,13 +3,13 @@ INSERT INTO url_mapping (long_url, short_url)
 VALUES ($1, $2) RETURNING *;
 
 -- name: GetUrl :one
-SELECT from url_mapping
+SELECT * FROM url_mapping
 WHERE long_url = $1 LIMIT 1;
 
 -- name: UpdateUrl :one
 UPDATE url_mapping SET short_url = $1
-WHERE long_url = $1 RETURNING *;
+WHERE long_url = $2 RETURNING *;
 
 -- name: DeleteUrl :exec
-DELETE from url_mapping
+DELETE FROM url_mapping
 WHERE long_url = $1;
--- internal/db/schema/schema.sql
+++ internal/db/schema/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE url_mapping (
+CREATE TABLE IF NOT EXISTS url_mapping (
                              id BIGINT PRIMARY KEY, -- Optional: A unique ID for each row
                              long_url TEXT NOT NULL UNIQUE, -- Column for storing long URLs, not null and unique
                              short_url VARCHAR(8) NOT NULL UNIQUE, -- Column for storing short URLs, not null and unique
